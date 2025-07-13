a2bc23e92d469341b568a8e3b0b0c4eff6761683
luca <luc335gs@gmail.com>
2025-07-13 17:31:29 -0300

    documents-route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './layouts/homeContent'
 import Navbar from './layouts/navbar'
+import Documents from './layouts/documents'
 import ContentRenderer from './layouts/contentRenderer'
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <Router>
     <Routes>
           <Route path="/" element={<ContentRenderer> <Home/></ContentRenderer>} />
+          <Route path="/documentos" element={<ContentRenderer> <Documents/></ContentRenderer>} />
     </Routes>
     </Router>
   )
