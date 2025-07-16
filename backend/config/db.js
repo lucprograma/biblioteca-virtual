3005584987f7d7e668a34340cb409360ec574b4b
luca <luc335gs@gmail.com>
2025-07-16 19:26:46 -0300

    docker-config-deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+#db = example
+#usuario = root
+#clave = db-btf5q
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
