1122e590977fb24fb8e07d7dfa3632d75dfb5115
Facundo Di Blasio <laparabellum1788@outlook.com>
2025-08-20 19:39:34 -0300

    branch-fixed

---
+++ backend/models/News.js
@@ -0,0 +1,15 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db.js';
+
+const News = sequelize.define('News', {
+  news_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: {type: DataTypes.STRING,allowNull: false},
+  content: { type: DataTypes.STRING, allowNull: false },
+  author_id: { type: DataTypes.INTEGER, allowNull: false },
+  published_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },  
+}, {
+  tableName: 'news',
+  timestamps: false,
+});
+
+export default News;
