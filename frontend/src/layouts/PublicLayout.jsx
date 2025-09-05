587d6133bca69a756b8bc023f7767627a0b53b37
luca <luc335gs@gmail.com>
2025-09-04 23:25:03 -0300

    fixed-bugs

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import Navbar from "../components/navbar";
+
+import Footer from "../components/footer"
 
 const PublicLayout = ({ children }) => {
   return (
