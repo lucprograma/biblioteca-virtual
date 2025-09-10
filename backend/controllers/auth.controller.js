ce2d68483b34343228684c32d7a8a4b26375a56a
luca <luc335gs@gmail.com>
2025-09-10 18:24:38 -0300

    login carnet & user list

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -75,7 +75,7 @@ export const getUnactive = async (req, res) => {
 //controlador get por id o todo
 export const getUsersAdmin = async (req, res) => {
   try {
-    const  user_id  = req.body?.user_id ||req.user?.user_id;
+    const  user_id  = req.body?.user_id || null;
     //console.log('ID recibido en getUsersAdmin:', user_id);
     if (user_id) {
       const user = await User.findByPk(user_id, {
