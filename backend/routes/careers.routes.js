2d79f6147474377d486386c4004d08cdabb2e8ba
luca <luc335gs@gmail.com>
2025-10-15 17:46:48 -0300

    routes-updated

--- backend/routes/careers.routes.js
+++ backend/routes/careers.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 //import validateSchema from '../middlewares/validateSchema.js';
 import chkToken from '../middlewares/checkToken.js';
-// import { getAllCareer, get_careers } from '../controllers/career.controller.js';
+import { getAllCareer } from '../controllers/career.controller.js';
 import checkAdmin from '../middlewares/checkAdmin.js';
 
 const router = express.Router();
 
 
-// router.get('/', chkToken, checkAdmin, get_careers)//trae todas las carreras
+router.get('/', getAllCareer)//trae todas las carreras
 //router.post('/tokenchk', chkToken, ()=>{console.log("pass")});
 
 
